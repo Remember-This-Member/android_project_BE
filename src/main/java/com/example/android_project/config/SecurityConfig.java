a10039a2b812613ea74ee8a0fded33a6bea7cc9a
seoyoungjin23 <ascd050@gmail.com>
2024-09-30 19:13:54 +0900

    [feat] CORS 설정 #1

--- src/main/java/com/example/android_project/config/SecurityConfig.java
+++ src/main/java/com/example/android_project/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import com.example.android_project.Util.JWTFilter;
 import com.example.android_project.Util.JWTUtil;
 import com.example.android_project.security.CustomSuccessHandler;
 import com.example.android_project.service.CustomOAuth2UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -12,6 +14,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -30,7 +34,6 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
         //csrf disable
         http
             .csrf((auth) -> auth.disable());
@@ -65,6 +68,28 @@ public class SecurityConfig {
         http
             .sessionManagement((session) -> session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        // cors 설정
+        http
+            .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                @Override
+                public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                    CorsConfiguration configuration = new CorsConfiguration();
+
+                    configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                    configuration.setAllowedMethods(Collections.singletonList("*"));
+                    configuration.setAllowCredentials(true);
+                    configuration.setAllowedHeaders(Collections.singletonList("*"));
+                    configuration.setMaxAge(3600L);
+
+                    configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+                    configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                    return configuration;
+                }
+            }));
 
         return http.build();
     }
