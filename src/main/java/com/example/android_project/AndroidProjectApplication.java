bbcb0ef9bc1429aa3c80d7b357da9fae82ae8b14
seoyoungjin23 <ascd050@gmail.com>
2024-09-30 18:38:28 +0900

    init commit

---
+++ src/main/java/com/example/android_project/AndroidProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.android_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndroidProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndroidProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/android_project/AndroidProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.android_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndroidProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
