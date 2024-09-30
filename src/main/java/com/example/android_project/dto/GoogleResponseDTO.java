616a56ea2bdcc7e64188cdacb0457f8965b72f94
seoyoungjin23 <ascd050@gmail.com>
2024-09-30 18:58:57 +0900

    [feat] 소셜 로그인(네이버, 구글) dto, service 생성 #1

---
+++ src/main/java/com/example/android_project/dto/GoogleResponseDTO.java
@@ -0,0 +1,37 @@
+package com.example.android_project.dto;
+
+import java.util.Map;
+
+public class GoogleResponseDTO implements OAuth2ResponseDTO{
+
+    private final Map<String, Object> attribute;
+
+    public GoogleResponseDTO(Map<String, Object> attribute) {
+
+        this.attribute = attribute;
+    }
+
+    @Override
+    public String getProvider() {
+
+        return "google";
+    }
+
+    @Override
+    public String getProviderId() {
+
+        return attribute.get("sub").toString();
+    }
+
+    @Override
+    public String getEmail() {
+
+        return attribute.get("email").toString();
+    }
+
+    @Override
+    public String getName() {
+
+        return attribute.get("name").toString();
+    }
+}
