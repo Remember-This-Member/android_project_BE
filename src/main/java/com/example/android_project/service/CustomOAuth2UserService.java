93a1847b39a5b797b6333ca2d83b84fe075f745d
seoyoungjin23 <ascd050@gmail.com>
2024-09-30 19:02:25 +0900

    [feat] 소셜 로그인(네이버, 구글) DB 저장 로직 추가 #1

--- src/main/java/com/example/android_project/service/CustomOAuth2UserService.java
+++ src/main/java/com/example/android_project/service/CustomOAuth2UserService.java
@@ -1,9 +1,11 @@
 package com.example.android_project.service;
 
+import com.example.android_project.UserRepository;
 import com.example.android_project.dto.GoogleResponseDTO;
 import com.example.android_project.dto.NaverResponseDTO;
 import com.example.android_project.dto.OAuth2ResponseDTO;
 import com.example.android_project.dto.UserDTO;
+import com.example.android_project.entity.UserEntity;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -13,36 +15,66 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
+    private final UserRepository userRepository;
+
+    public CustomOAuth2UserService(UserRepository userRepository) {
+
+        this.userRepository = userRepository;
+    }
+
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 
         OAuth2User oAuth2User = super.loadUser(userRequest);
-
-        System.out.println(oAuth2User);
+        //System.out.println(oAuth2User);
 
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
-        OAuth2ResponseDTO oAuth2Response = null;
+        OAuth2ResponseDTO oAuth2ResponseDTO = null;
         if (registrationId.equals("naver")) {
 
-            oAuth2Response = new NaverResponseDTO(oAuth2User.getAttributes());
+            oAuth2ResponseDTO = new NaverResponseDTO(oAuth2User.getAttributes());
         }
         else if (registrationId.equals("google")) {
 
-            oAuth2Response = new GoogleResponseDTO(oAuth2User.getAttributes());
+            oAuth2ResponseDTO = new GoogleResponseDTO(oAuth2User.getAttributes());
         }
         else {
 
             return null;
         }
+        String username = oAuth2ResponseDTO.getProvider()+" "+oAuth2ResponseDTO.getProviderId();
+        UserEntity existData = userRepository.findByUsername(username);
 
-        //리소스 서버에서 발급 받은 정보로 사용자를 특정할 아이디값을 만듬
-        String username = oAuth2Response.getProvider()+" "+oAuth2Response.getProviderId();
+        if (existData == null) {
 
-        UserDTO userDTO = new UserDTO();
-        userDTO.setUsername(username);
-        userDTO.setName(oAuth2Response.getName());
-        userDTO.setRole("ROLE_USER");
+            UserEntity userEntity = new UserEntity();
+            userEntity.setUsername(username);
+            userEntity.setEmail(oAuth2ResponseDTO.getEmail());
+            userEntity.setName(oAuth2ResponseDTO.getName());
+            userEntity.setRole("ROLE_USER");
 
-        return new CustomOAuth2User(userDTO);
+            userRepository.save(userEntity);
+
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUsername(username);
+            userDTO.setName(oAuth2ResponseDTO.getName());
+            userDTO.setRole("ROLE_USER");
+
+            return new CustomOAuth2User(userDTO);
+        }
+        else {
+
+            existData.setEmail(oAuth2ResponseDTO.getEmail());
+            existData.setName(oAuth2ResponseDTO.getName());
+
+            userRepository.save(existData);
+
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUsername(existData.getUsername());
+            userDTO.setName(oAuth2ResponseDTO.getName());
+            userDTO.setRole(existData.getRole());
+
+            return new CustomOAuth2User(userDTO);
+        }
     }
 }
