f68422851380a9046bbcabbc95d0339f8a263e62
seoyoungjin23 <ascd050@gmail.com>
2024-10-06 20:53:41 +0900

    [feat] User entity 기능 추가 #1
    UniqueConstraint 어노테이션을 활용해서 email과 provider의 조합이 중복되지 않는지 검사

--- src/main/java/com/example/android_project/entity/User.java
+++ src/main/java/com/example/android_project/entity/User.java
@@ -1,11 +1,14 @@
 package com.example.android_project.entity;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,18 +19,25 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(uniqueConstraints = {
+    @UniqueConstraint(name = "UniqueProviderAndEmail", columnNames = {"provider", "email"})
+})
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String provider;
 
+    @Column(nullable = false)
     private String providerId;
 
+    @Column(nullable = false)
     private String email;
 
+    @Column(nullable = false)
     private String password;
 
     @OneToOne(cascade = CascadeType.ALL)
